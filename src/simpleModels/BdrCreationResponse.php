043d75513c6e61919cec70c23b9b281e7872ce4e
Jonathan Hopkins <jhopkins@whitelabelcomm.com>
2016-06-10 07:33:15 -0400

    Have both Info and Location in Bdr

--- src/simpleModels/BdrCreationResponse.php
+++ src/simpleModels/BdrCreationResponse.php
@@ -6,6 +6,7 @@ class BdrCreationResponse {
     use BaseModel;
 
     protected $fields = array(
+        "Info" => array("type" => "string"),
         "Location" => array("type" => "string"),
     );
     public function __construct($data) {
