03984a294b315e0ff6e06d8b1f386a126c87dd70
Dan Tolbert <dtolbert@bandwidth.com>
2017-07-13 15:14:54 -0400

    update types

--- src/simpleModels/Subscriber.php
+++ src/simpleModels/Subscriber.php
@@ -9,6 +9,9 @@ class Subscriber {
         "SubscriberType" => array("type" => "string"),
         "BusinessName" => array("type" => "string"),
         "ServiceAddress" => array("type" => "\Iris\ServiceAddress"),
+        "FirstName" => array("type" => "string"),
+        "LastName" => array("type" => "string"),
+        "MiddleInitial" => array("type" => "string")
     );
 
     public function __construct($data) {
